f5d404a4894e1ba9a7c8be6d90865acb8a1c4961
harsh6183 <harshnimbalkar6181@gmail.com>
2026-03-04 15:45:06 +0530

    Controller class end point checkup change

--- email-writer-sb/src/main/java/com/email/writer/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/EmailGeneratorController.java
@@ -1,21 +1,23 @@
 package com.email.writer;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
 @CrossOrigin(origins="*")
 public class EmailGeneratorController {
+
     private final EmailGeneratorService emailGeneratorService;
+
+    // Test endpoint
+    @GetMapping("/")
+    public String test() {
+        return "Smart AI Email Backend is Live 🚀";
+    }
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
